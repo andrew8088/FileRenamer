695438a7873c9779b68c69b4dbc52cf30b5e5a97
Andrew Burgess <andrew8088@gmail.com>
2011-05-02 21:51:39 -0400

    initial commit: FileRenamer added.

---
+++ filerenamer.rb
@@ -0,0 +1,29 @@
+class FileRenamer
+  attr_accessor :regex
+  def initialize regex, path = Dir.pwd
+    @regex = regex
+    @path  = path
+  end
+
+  def rename_files
+    Dir.glob( File.join(@path, "*")).each_with_index do |file, i|
+      file = File.basename(file)
+      unless (md = file.match regex).nil?
+        rename file, yield(md,i)
+      end
+    end
+    puts "Successfully renamed all files that matched!"
+  end
+
+  def rename_file_in path
+    old_path = @path
+    rename_files
+    @path = old_path
+  end
+
+  private
+
+  def rename old, new
+    File.rename(File.join(@path, old), File.join(@path, new))
+  end
+end
